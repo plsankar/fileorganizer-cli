67d8d62f21ef8a10b05a15b7de3bc80973b7471e
plsankar <plsankar1996@gmail.com>
2021-07-17 16:42:29 +0530

    perf: Added logger empty gaurd

--- logger.js
+++ logger.js
@@ -5,11 +5,17 @@ const path = require("path");
 let logContent = "";
 
 module.exports.log = (content) => {
+    if (content === "") {
+        return;
+    }
     console.log(content);
     logContent += `${content.replace(/\u001b\[.*?m/g, "")}\n`;
 };
 
 module.exports.generateLogFile = (workingDir) => {
+    if (logContent === "") {
+        return;
+    }
     const filename = `fileorganizer-${moment().format("YYYY-MM-DD-HH-m-s")}.log`;
     const logPath = path.resolve(workingDir, filename);
     writeFileSync(logPath, logContent);
